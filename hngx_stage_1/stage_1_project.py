099cabbb55721f68bb236c7cee6a7fb115ba90c6
habiibullah <ajiboyhabiib@gmail.com>
2023-09-07 09:54:37 +0100

    get info

--- hngx_stage_1/stage_1_project.py
+++ hngx_stage_1/stage_1_project.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/get_info', method=['GET'])
+@app.route('/get_info', methods=['GET'])
 
 def get_info():
+
+    slack_name = 'Ajiboye Habiibullah'
+    track = 'Backend'
 
     # Get the query parameters from the request
 
@@ -20,12 +23,12 @@ def get_info():
     # Perform some processing based on the parameters
 
     result = {
-            "slack_name": "ajiboye_habiibullah",
+            "slack_name": slack_name,
             "current_day": "Thursday",
             "utc_time": "2023-09-07T08:34:57Z",
-            "track" : "backend",
-            "github_file_url":"",
-            "github_repo_url":"",
+            "track" : track,
+            "github_file_url":"https://github.com/habiibullah/hngx_backend_track/blob/main/hngx_stage_1/stage_1_project.py",
+            "github_repo_url":"https://github.com/habiibullah/hngx_backend_track",
             "status_code": 200
             }
     return jsonify(result)
